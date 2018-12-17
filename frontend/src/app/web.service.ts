1476d4001c9169a3c8f0fe5df6fb78c49e42b1cf
emyhoang <emyhoang8899@gmail.com>
2018-12-17 14:50:57 -0800

    handled errors with try or catch in service file

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 // import 'rxjs/add/operator/toPromise';
+import { MatSnackBar } from '@angular/material';
+
 
 
 
@@ -11,21 +13,33 @@ export class WebService {
   BASE_URL = 'http://localhost:63145/api'
   messages = [];
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private sb: MatSnackBar) {
     this.getMessages();
   }
 
   async getMessages() {
-    var response = await this.http.get(this.BASE_URL + '/messages').toPromise();
-    this.messages = response.json();
+    try {
+      var response = await this.http.get(this.BASE_URL + '/messages').toPromise();
+      this.messages = response.json();
+    } catch (error) {
+      this.handleError("Unable to get messages");
+    }
   }
 
   async postMessage(message) {
-    var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
-    this.messages.push(response.json());
+    try {
+      var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
+      this.messages.push(response.json());
+    } catch (error) {
+      this.handleError("Unable to post message");
+    }
+  }
+
+  private handleError(error) {
+    console.error(error);
+    this.sb.open(error, 'close', { duration: 2000 });
   }
 }
-
 
 
 

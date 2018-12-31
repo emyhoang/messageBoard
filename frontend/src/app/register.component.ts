4cf147b39f5767e1e400cf7fd7861afa272f06b0
emyhoang <emyhoang8899@gmail.com>
2018-12-31 15:56:36 -0800

    added password and confirm password validation with a custom validator

--- frontend/src/app/register.component.ts
+++ frontend/src/app/register.component.ts
@@ -14,20 +14,25 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   form;
-    constructor(private fb: FormBuilder) {
-        this.form = fb.group({
-          firstName: ['', Validators.required],
-          lastName: ['', Validators.required],
-          email: ['', Validators.required],
-          password: ['', Validators.required],
-          confirmPassword: ['', Validators.required]
-        })
-    }
-    onSubmit() {
-        console.log(this.form.valid);
-    }
-    isValid(control){
-      return this.form.controls[control].invalid && this.form.controls[control].touched
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: matchingFields('password', 'confirmPassword') })
+  }
+  onSubmit() {
+    console.log(this.form.errors);
+  }
+  isValid(control) {
+    return this.form.controls[control].invalid && this.form.controls[control].touched
+  }
+  let matchingFields = (field1, field2) => {
+  return form => {
+    if (form.controls[field1].value !== form.controls[field2].value)
+      return { mismatchedFields: true }
   }
 
- }
+}
